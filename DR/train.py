f8b07e0cf21c55c691904380f23375814b2dd0c9
PrakashR <vanapaliprakash@gmail.com>
2016-12-12 19:56:25 +0530

    changed code from train_on_batch to fit_generator
    -We need to shift from train_on_batch to fit_generator as this is more flexible and reduces your code to one line from present 10+ lines. 
    -It also helps in reading Images on CPU and training network on GPU parallelly, which saves a lot of time.

--- DR/train.py
+++ DR/train.py
@@ -61,12 +61,15 @@ x_train,x_test,y_train, y_test = train_test_split(x_train,y_train,
 x_valid = np.array([cv2.imread("/data/dr/data/sample_270_270/"+i+".jpeg") for i in y_valid.index])
 y_valid = np.array(pd.get_dummies(y_valid["level"]))
 
+model_train = model((270,270,3))
+
 """
 
 Once you get x_train,x_test and x_valid. Train the data as you need and test on validation 
 
 """
 
+"""
 epoch = 10
 
 train_acc = []
@@ -82,6 +85,14 @@ for ep in range(epoch):
 		test_loss = model_train.test_on_batch(x_valid,x_valid)
 		print(loss)
 
-		
-# We need to shift from train_on_batch to fit_generator as this is more flexible and reduces your code to one line from present 
-# 10+ lines . It also helps in reading Images on cpu and training network on GPU parallely, which saves a lot of time.
+"""
+
+# writing a fit_generator function instead of train_on_batch function 
+model_train.fit_generator(data_generator(x_train,location), samples_per_epoch=len(x_train), 
+	      nb_epoch=20, verbose=1, callbacks=[], validation_data=(x_valid,y_valid), 
+	      class_weight=None, max_q_size=10, nb_worker=1, pickle_safe=False)
+
+model_train.save("model_arc_iter_num.h270")
+model_train.save_weights("model_weight_iter_num.h5")
+
+# code yet haven't checked weather running or not 
