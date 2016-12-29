a1b0c6120d233152625954e0f086342506689eaa
PrakashR <vanapaliprakash@gmail.com>
2016-12-29 16:01:27 +0530

    Adding valid_Image_generator and train_test split functions

--- Kaggle/tensorflow/helper.py
+++ Kaggle/tensorflow/helper.py
@@ -13,23 +13,25 @@ import random
 from PIL import Image
 import os
 from sklearn.utils import shuffle
+from tqdm import tqdm
+from pre_process import *
 
 import warnings
 warnings.filterwarnings("ignore")
 
 # create a dataframe of Image_labels along with Name
-labels = pd.DataFrame
-folderlist = glob.glob("/Users/Satish/Downloads/kaggle-fish/data/*")
-
-for i in range(len(folderlist)):
-    filelist = glob.glob(folderlist[i]+"/*")
-    for f in range(len(filelist)):
-        index = filelist[f].rsplit("/")[-2]+"_"+filelist[f].rsplit("/")[-1]
-        level = filelist[f].rsplit("/")[-2]
-        labels = labels.append({"index":index,"level":level}, ignore_index=True)
-
-
-labels.to_csv("labels.csv",sep=",")
+# labels = pd.DataFrame
+# folderlist = glob.glob("/Users/Satish/Downloads/kaggle-fish/data/*")
+#
+# for i in range(len(folderlist)):
+#     filelist = glob.glob(folderlist[i]+"/*")
+#     for f in range(len(filelist)):
+#         index = filelist[f].rsplit("/")[-2]+"_"+filelist[f].rsplit("/")[-1]
+#         level = filelist[f].rsplit("/")[-2]
+#         labels = labels.append({"index":index,"level":level}, ignore_index=True)
+#
+#
+# labels.to_csv("labels.csv",sep=",")
 
 """
 Plan
@@ -59,14 +61,37 @@ def filelist(folder):
     return file_dict
 
 
+""" Function to split data into train and test """
 
-def Image_data_generator(folderlist,labels,resize = (920,1200),Transformation = True, scaling = True, batch_size = 16):
+def train_test(folderlist, test_size=0.3, random_state = 0):
+    """
+    Returns two dict objects.
+
+    Args:
+    folderlist: A dict which contains the label as key and its respective Images locations as a list in place of value.
+    test_size: size of the test data required
+    random_state: seed required
+
+    Returns
+    Splits folderlist into two dictionaries and returns them as train and test
+
+    """
+    random.seed(random_state)
+    test = {}
+    train = {}
+    for i in folderlist.keys():
+        test[i] = [random.choice(folderlist[i]) for x in range(int(len(folderlist[i])*test_size))]
+        train[i] = [x for x in folderlist[i] if x not in test[i]]
+    return train,test
+
+
+
+def Dev_Image_data_generator(folderlist,resize = (920,1200),Transformation = True, scaling = True, batch_size = 16):
     """
     Yields a tuple (x,y) with x - batch of images(numpy array), y - image label (numpy array)
 
     Args:
     folderlist : A dictionary object
-    labels : pandas dataframe with index as labels name
     resize : tuple of (x,y)
     Transformation : If True Data Aguementation is done
     scaling : If True , every Image is scaled
@@ -78,10 +103,10 @@ def Image_data_generator(folderlist,labels,resize = (920,1200),Transformation = 
 
     while True:
         total_classes = len(folderlist.keys())
-        keys = folderlist.keys[]
+        keys = folderlist.keys()
         Images = []
         Image_label = []
-        for key in foldelist.keys():
+        for key in folderlist.keys():
             img_label = random.choice(folderlist[key])
             img = Image.open(img_label,'r')
             h = resize[1]
@@ -92,19 +117,22 @@ def Image_data_generator(folderlist,labels,resize = (920,1200),Transformation = 
             bg_w, bg_h = background.size
             offset = ((bg_w - img_w) / 2, (bg_h - img_h) / 2)
             background.paste(img, offset)
-            Images.append(background)
+            background = np.asarray(background)
             if Transformation == True:
-                rotation = np.array([rotate(background[j],random.choice(range(360))) for j in range(1)])
+                rotation = rotate(background,random.choice(range(360)))
                 translate = translate_xy(background,random.choice(range(resize[0]/4)),random.choice(range(resize[1]/4)))
-                flip = np.array([cv2.flip(rotation[j],1) for j in range(len(rotation))])
-                Y = np.concatenate((rotation,flip,translate))
+                flip = cv2.flip(rotation,1)
+                Y = np.concatenate((rotation[np.newaxis,:,:,:],flip[np.newaxis,:,:,:],translate[np.newaxis,:,:,:]))
                 Images.append(Y)
                 Images.append(background[np.newaxis,:,:,:])
-                Image_label.append([key for i in range(len(Images))])
+                Image_label.append([key for i in range(4)])
+            else:
+                Images.append(background[np.newaxis,:,:,:])
+                Image_label.append([key])
         Image_label = np.concatenate(Image_label)
         Images = np.concatenate(Images)
         Image_label = np.array(pd.get_dummies(Image_label))
-        X_Image,Y_Image = shuffle(Images,Image_label,random_state=0)
+        X_Image , Y_Image = shuffle(Images,Image_label,random_state=0)
         if scaling == True:
             X_Image = X_Image/255
         else:
@@ -114,3 +142,58 @@ def Image_data_generator(folderlist,labels,resize = (920,1200),Transformation = 
             x = X_Image[batch*batch_size:(batch+1)*batch_size,:,:,:]
             y = Y_Image[batch*batch_size:(batch+1)*batch_size]
             yield((x,y))
+
+
+def Valid_Image_data_generator(folderlist,resize = (920,1200),Transformation = True, scaling = True):
+    """
+    Yields a tuple (x,y) with x - batch of images(numpy array), y - image label (numpy array)
+
+    Args:
+    folderlist : A dictionary object
+    resize : tuple of (x,y)
+    Transformation : If True Data Aguementation is performed.
+    scaling : If True , every Image is scaled by 255
+    batch_size : The batch_size to yield for every iteration
+
+    returns:
+    A tuple with Images and labels as numpy arrays.
+    """
+
+    while True:
+        total_classes = len(folderlist.keys())
+        keys = folderlist.keys()
+        Images = []
+        Image_label = []
+        for key in folderlist.keys():
+            for j in range(len(folderlist[key])):
+                img_label = folderlist[key][i]
+                img = Image.open(img_label,'r')
+                h = resize[1]
+                l = int(img.size[1]*h/img.size[0])
+                img = img.resize((h,l), Image.ANTIALIAS)
+                background = Image.new('RGB', (resize[1], resize[0]), (255, 255, 255))
+                img_w, img_h = img.size
+                bg_w, bg_h = background.size
+                offset = ((bg_w - img_w) / 2, (bg_h - img_h) / 2)
+                background.paste(img, offset)
+                background = np.asarray(background)
+                if Transformation == True:
+                    rotation = rotate(background,random.choice(range(360)))
+                    translate = translate_xy(background,random.choice(range(resize[0]/4)),random.choice(range(resize[1]/4)))
+                    flip = cv2.flip(rotation,1)
+                    Y = np.concatenate((rotation[np.newaxis,:,:,:],flip[np.newaxis,:,:,:],translate[np.newaxis,:,:,:]))
+                    Images.append(Y)
+                    Images.append(background[np.newaxis,:,:,:])
+                    Image_label.append([key for i in range(4)]) # Four because we are doing rot,trans,flip and one original Image
+                else:
+                    Images.append(background[np.newaxis,:,:,:])
+                    Image_label.append([key])
+        Image_label = np.concatenate(Image_label)
+        Images = np.concatenate(Images)
+        Image_label = np.array(pd.get_dummies(Image_label))
+        X_Image , Y_Image = shuffle(Images,Image_label,random_state=0)
+        if scaling == True:
+            X_Image = X_Image/255
+        else:
+            X_Image = X_Image
+        return (X_Image,Y_Image)
